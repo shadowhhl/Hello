d37b488201ae86138229188a8c096230681610bc
shadowhhl <shadowhhl@hotmail.com>
2013-04-11 19:13:09 -0400

    2

--- Hello/src/test/Main.java
+++ Hello/src/test/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("hello from macbook");
 	}
 
 }
